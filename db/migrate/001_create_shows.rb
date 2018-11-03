3c0c6b1c324fbe584cb1f8ef3f2bf6c5b5311d5c
Justin Kenney <justinkenney13@gmail.com>
2018-11-03 20:13:00 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,7 @@ class CreateShows < ActiveRecord::Migration
     create_table :shows do |t|
       t.string :name
       t.string :day
-      t.string :network      
+      t.string :network
       t.integer :rating
     end
   end
